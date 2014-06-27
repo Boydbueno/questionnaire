a7a7a42dc0958dc5be531ae230b40c3faaa6da6f
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-06-28 00:46:53 +0200

    Updated files to follow style guide

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,25 +9,27 @@
  * Main module of the application.
  */
 angular
-  .module('cardGameApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('cardGameApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'ui.bootstrap'
+    ])
+    .config(function ($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,10 @@
  * Controller of the cardGameApp
  */
 angular.module('cardGameApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('AboutCtrl', function ($scope) {
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
