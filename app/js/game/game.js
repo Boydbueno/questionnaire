1c21b28004cdd420c7a8a9b0a7e48cec0b2315cb
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-07-01 01:49:08 +0200

    Added some basic question test

--- app/js/game/game.js
+++ app/js/game/game.js
@@ -1,21 +1,46 @@
-'use strict';
+(function() {
 
-/**
- * @ngdoc function
- * @name cardGameApp.controller:GameCtrl
- * @description
- * # GameCtrl
- * Controller of the cardGameApp
- */
-var app = angular.module('cardGameApp');
+    'use strict';
 
-app.controller('GameCtrl', function () {
+    /**
+     * @ngdoc function
+     * @name cardGameApp.controller:GameCtrl
+     * @description
+     * # GameCtrl
+     * Controller of the cardGameApp
+     */
+    var app = angular.module('cardGameApp');
 
-})
-    .config(function ($routeProvider) {
-        $routeProvider
-            .when('/game', {
-                templateUrl: 'js/game/game.html',
-                controller: 'GameCtrl'
-            });
-    });
+    app.controller('GameCtrl', function() {
+        this.cards = cards;
+    })
+        .config(function($routeProvider) {
+            $routeProvider
+                .when('/game', {
+                    templateUrl: 'js/game/game.html',
+                    controller: 'GameCtrl'
+                });
+        });
+
+    var cards = [
+        {
+            question: "Are we human?",
+            category: "Weird",
+            answers: [
+                { answer: "Hell no!" },
+                { answer: "Ofcourse we are!" },
+                { answer: "Nop, we're dancer.", correct: true }
+            ]
+        },
+        {
+            question: "What is equal to 1+1?",
+            category: "Math",
+            answers: [
+                { answer: "11!" },
+                { answer: "4-2!", correct: true },
+                { answer: "3" }
+            ]
+        }
+    ];
+
+})();
