6d67cc33fdd4c7895f9e17d88027d5f54e07188e
Boyd Bueno de Mesquita <boydbuenodemesquita@gmail.com>
2014-07-02 00:43:34 +0200

    Added function for showing gender inside player model

--- app/js/factories/Player.js
+++ app/js/factories/Player.js
@@ -1,14 +1,20 @@
-(function(){
+(function () {
     "use strict";
 
     var app = angular.module('cardGameApp');
 
-    app.factory('Player', [function() {
+    app.factory('Player', [function () {
         function Player(playerData) {
-            if(playerData) {
+            if (playerData) {
                 this.setData(playerData);
             }
         }
+
+        Player.prototype = {
+            genderIcon: function () {
+                return (this.gender == 'male') ? "&#9792;" : "&#9794;";
+            }
+        };
 
         return Player;
     }]);
